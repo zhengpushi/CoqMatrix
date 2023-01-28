67db7501adc13e992b76ed6ab1834e3506dd864f
ZhengPu Shi <zhengpushi@nuaa.edu.cn>
2023-01-28 22:59:58 +0800

    examples for usage

---
+++ CoqMatrix/examples.v
@@ -0,0 +1,101 @@
+(*
+  Copyright 2022 ZhengPu Shi
+  This file is part of CoqMatrix. It is distributed under the MIT
+  "expat license". You should have recieved a LICENSE file with it.
+
+  purpose   : Examples for usage of CoqMatrix library.
+  author    : ZhengPu Shi
+  date      : 2023.01
+*)
+
+From CoqMatrix Require Import MatrixAll.
+
+(** * Matrix over natural numbers *)
+Module Example4Nat.
+  (** import the library needed to use *)
+  Import MatrixNat_DR. (* model: DepRec *)
+
+  (** Then, all functions, theorems, notations are available *)
+  Example dl := [[1;2;3];[4;5;6]].
+  Example m1 : mat 2 3 := @l2m 2 3 dl.
+  Compute (m2l m1). (* = [[1; 2; 3]; [4; 5; 6]] : list (list A) *)
+  Compute (mnth m1 0 1). (* = 2 : A *)
+  Compute (m2l (m1\T)). (* = [[1; 4]; [2; 5]; [3; 6]] : list (list A) *)
+  Goal mmap S m1 == l2m [[2;3;4];[5;6;7]].
+  Proof. cbv. reflexivity. Qed.
+
+  (** Check that if A is nat really *)
+  Print A. (* A = nat : Type *)
+
+  (** You can mixed use all models *)
+  Import MatrixAllNat. (* all models *)
+  Compute @DL.l2m 2 3 dl.
+  Compute @DP.l2m 2 3 dl.
+  Compute @DR.l2m 2 3 dl.
+  Compute @NF.l2m 2 3 dl.
+End Example4Nat.
+
+
+(** * Matrix over rational numbers *)
+Module Example4Q.
+
+  (** Import library needed *)
+  Import MatrixQ_DL. (* model: DepList *)
+  Open Scope Q.
+  Open Scope mat_scope.
+
+  (** Q is setoid equal, not leibniz equal *)
+  Goal 6/4 <> 1.5. Proof. easy. Qed.
+  Goal (6/4 == 1.5)%Q. Proof. easy. Qed.
+
+  (** We can then see that our matrix theory works well for elements of rational 
+      type due to its support for setoid equality. *)
+  Example m1 : mat 2 3 := l2m [[6/4;10/4;7/2];[4;5;6]].
+  Example m2 : mat 2 3 := l2m [[1.5;2.5;3.5];[4;5;6]].
+  Example eq0 : m1 <> m2. Proof. easy. Qed.
+  Example eq1 : m1 == m2. Proof. repeat constructor. Qed.
+
+  (** Proof by compute or by using properties *)
+  Definition mat1 {n} := mat1 n.
+  Goal m1 * mat1 == m2. Proof. cbn;repeat constructor. Qed.
+  Goal m1 * mat1 == m2. Proof. rewrite mmul_1_r. apply eq1. Qed.
+  
+End Example4Q.
+
+
+(** * Mutual conversion and mixed use between multiple models *)
+Module Example4Cvt.
+  
+  (** Import library needed to use *)
+  Import MatrixAllZ. (* all models *)
+  Import MatrixZ_DL. (* notations on DL *)
+  Import Coq.Vectors.Vector VectorNotations. (* notations for Coq.Vector *)
+  Open Scope Z.
+  Open Scope mat_scope.
+
+  (** convert from one model to other models *)
+  Example m : NF.mat 2 2 := NF.mk_mat_2_2 1 2 3 4.
+  Compute nf2dl m. (* = [[1; 2]; [3; 4]]%vector : DL.mat 2 2 *)
+  Compute nf2dp m. (* = (1, (2, tt), (3, (4, tt), tt)) : DP.mat 2 2 *)
+  Compute nf2dr m. (* = {| mdata := [[1;2];[3;4]]; .. |} : DR.mat 2 2 *)
+  (* Compute nf2ff m. *)
+  (* The evaluation on FF is crashed! *)
+
+  (** prove that {DL -> DP -> DR -> NF -> DL} return back *)
+  Goal forall r c (m1 : DL.mat r c),
+    let m2 : DP.mat r c := dl2dp m1 in
+    let m3 : DR.mat r c := dp2dr m2 in
+    let m4 : NF.mat r c := dr2nf m3 in
+    let m5 : DL.mat r c := nf2dl m4 in
+    m5 == m1.
+  Proof.
+    intros. unfold m5,m4,m3,m2,dl2dp,dp2dr,dr2nf,nf2dl.
+    rewrite NF.m2l_l2m_id; auto with mat.
+    rewrite DP.m2l_l2m_id; auto with mat.
+    rewrite DR.m2l_l2m_id; auto with mat.
+    rewrite DL.l2m_m2l_id; auto with mat.
+    reflexivity.
+  Qed.
+  
+End Example4Cvt.
+
